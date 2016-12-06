5b6fc148eccdc3b96d4700b6b0a2932e2784b419
David Seek <davidseek1986@gmail.com>
2016-12-06 21:33:17 +0100

    init commit

---
+++ SpotifySwiftStartingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifySwiftStartingProject
+//
+//  Created by David Seek on 12/6/16.
+//  Copyright © 2016 David Seek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var ctrl: SPTAudioStreamingController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
